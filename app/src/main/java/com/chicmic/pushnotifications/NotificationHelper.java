a0f5396d48a3a657bb90533e6fce09c7cb9908f1
Parminder Singh <parminder.singh@chicmic.co.in>
2019-05-03 18:48:02 +0530

    2nd commit

--- app/src/main/java/com/chicmic/pushnotifications/NotificationHelper.java
+++ app/src/main/java/com/chicmic/pushnotifications/NotificationHelper.java
@@ -1,6 +1,8 @@
 package com.chicmic.pushnotifications;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 
@@ -10,10 +12,17 @@ public class NotificationHelper {
 
     public static void displayNotifications(Context context,String title,String body)
     {
+
+        Intent intent=new Intent(context,ProfileActivity.class);
+        PendingIntent pendingIntent=PendingIntent.getActivity(context,100,intent,PendingIntent.FLAG_CANCEL_CURRENT);;
+
+
         NotificationCompat.Builder mBuilder=new NotificationCompat.Builder(context,CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_icon)
                 .setContentTitle(title)
                 .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         NotificationManagerCompat notificationManagerCompat=NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(1,mBuilder.build());
